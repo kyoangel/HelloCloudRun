fe1428e98ff83a07bad435390f1fbb9c63705efd
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2020-10-18 21:41:57 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloCloudRun
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            var url = string.Concat("http://0.0.0.0:", port);
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().UseUrls(url);
+
+                });
+        }
+    }
+}
